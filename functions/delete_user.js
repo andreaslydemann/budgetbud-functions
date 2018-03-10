e58e1557a1450a238a6b21d21fad564d28b49b85
Andreas Lüdemann <andreaslydemann@gmail.com>
2018-03-10 22:43:28 +0100

    Add verification of jwt before user deletion

--- functions/delete_user.js
+++ functions/delete_user.js
@@ -1,22 +1,28 @@
 const admin = require('firebase-admin');
+const cors = require('cors')({origin: true});
 
 module.exports = function (req, res) {
-    if (!req.body.cprNumber) {
-        return res.status(400).send({error: 'CPR-nummer ikke modtaget.'});
-    }
+    cors(req, res, () => {
+        const token = req.get('Authorization').split('Bearer ')[1];
 
-    const cprNumber = String(req.body.cprNumber);
+        admin.auth().verifyIdToken(token)
+            .then(() => {
+                if (!req.body.cprNumber)
+                    return res.status(400).send({error: 'CPR-nummer ikke modtaget.'});
 
-    admin.auth().deleteUser(cprNumber)
-        .then(() => {
-            const db = admin.firestore();
+                const cprNumber = String(req.body.cprNumber);
 
-            db.collection("users").doc(cprNumber).delete()
-                .then(() => {
-                    res.status(200).send({success: true});
-                })
-                .catch(err => res.status(422)
-                    .send({error: 'Bruger blev slettet, men tilknyttet data blev ikke.'}));
-        })
-        .catch(err => res.status(422).send({error: 'Sletning af bruger fejlede.'}));
+                admin.auth().deleteUser(cprNumber)
+                    .then(() => {
+                        const db = admin.firestore();
+
+                        db.collection("users").doc(cprNumber).delete()
+                            .then(() => res.status(200).send({success: true}))
+                            .catch(err => res.status(422)
+                                .send({error: 'Bruger blev slettet, men tilknyttet data blev ikke.'}));
+                    })
+                    .catch(err => res.status(422).send({error: 'Sletning af bruger fejlede.'}));
+            })
+            .catch(err => res.status(401).send({error: err}));
+    });
 };
