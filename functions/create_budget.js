62fd68f508226e5767a92a4a9c7c3d23215e0584
VictorBusk <victorbusk@hotmail.com>
2018-03-20 15:31:33 +0100

    Merged the create functions for budget

--- functions/create_budget.js
+++ functions/create_budget.js
@@ -7,24 +7,37 @@ module.exports = function (req, res) {
         admin.auth().verifyIdToken(token)
             .then(() => {
                 // Verify that the user provided an income
-                if (!req.body.income)
-                    return res.status(422).send({error: 'Indkomst er ikke et gyldigt beløb.'});
+                if (!req.body.income || !req.body.userID || !req.body.categories)
+                    return res.status(422).send({error: 'Fejl i indtastningen.'});
 
-                if (!req.body.cprNumber)
-                    return res.status(422).send({error: 'CPR-nummer ikke modtaget.'});
-
-                const cprNumber = String(req.body.cprNumber);
-                const income = String(req.body.income);
                 const db = admin.firestore();
+                const userID = String(req.body.userID);
+                const income = String(req.body.income);
+                const totalCategories = String(req.body.categories.length);
+                const budgetID = String(req.body.budgetID);
+                const categories = req.body.categories;
 
                 // Create a new budget using the income and category
                 db.collection('budgets').doc().set({
-                    income: income,
-                    userID: cprNumber
+                    income,
+                    userID
                 })
                     .then(() => res.status(200).send({success: true}))
                     .catch(err => res.status(422)
                         .send({error: 'Kunne ikke oprette budget.'}));
+
+                for (let i = 0; i < totalCategories; i++) {
+                    let categoryName = String(categories[i].name);
+                    let categoryAmount = String(categories[i].amount);
+                    db.collection('categories').doc().set({
+                        name: categoryName,
+                        amount: categoryAmount,
+                        budgetID
+                    })
+                        .then(() => res.status(200).send({success: true}))
+                        .catch(err => res.status(422)
+                            .send({error: 'Kunne ikke oprette kategori.'}));
+                }
             })
             .catch(err => res.status(401).send({error: err}));
     })
