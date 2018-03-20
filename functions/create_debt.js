7a6f39b5d8c67f72ac2a968b2c1e9ac9f107e884
Andreas Lüdemann <andreaslydemann@gmail.com>
2018-03-20 12:04:46 +0100

    Changes to create debt function

--- functions/create_debt.js
+++ functions/create_debt.js
@@ -9,28 +9,41 @@ module.exports = function (req, res) {
                 if (!req.body.name || !req.body.totalAmount || !req.body.budgetID)
                     return res.status(422).send({error: 'Fejl i indtastning.'});
 
-                if (!req.body.expirationDate || Date.now() >= req.body.expirationDate.toDate())
+                if (!req.body.expirationDate || Date.now() >= new Date(req.body.expirationDate))
                     return res.status(422).send({error: 'Ugyldig udløbsdato.'});
 
-                if(!req.body.categories.length || req.body.categories.length > 0)
+                if (!req.body.categories || req.body.categories.length === 0)
                     return res.status(422).send({error: 'Ingen kategorier valgt.'});
 
                 const name = String(req.body.name);
                 const totalAmount = parseInt(req.body.totalAmount);
                 const budgetID = String(req.body.budgetID);
                 const expirationDate = new Date(req.body.expirationDate);
-                //const categories = req.body.categories;
+                const categories = req.body.categories;
 
                 const db = admin.firestore();
 
-                db.collection('debts').doc().set({
+                const doc = db.collection('debts').doc().set({
                     name: name,
-                    totalAmount: totalAmount,
                     budgetID: budgetID
                 })
-                    .then(() => res.status(200).send({success: true}))
-                    .catch(err => res.status(422)
-                        .send({error: 'Kunne ikke oprette gæld.'}));
+                    .then(() => {
+                        /*/
+                        for (let i = 0; i < totalCategories; i++) {
+                            let categoryName = String(req.body.category[i].name);
+                            let categoryAmount = String(req.body.category[i].amount);
+                            db.collection('categories').doc().set({
+                                name: categoryName,
+                                amount: categoryAmount,
+                                budgetID: budgetID
+                            })
+                                .then(() => res.status(200).send({success: true}))
+                                .catch(err => res.status(422)
+                                    .send({error: 'Kunne ikke oprette kategori.'}));
+                        }*/
+                        res.status(200).send({success: true});
+                    })
+                    .catch(err => res.status(422).send({error: 'Kunne ikke oprette gæld.'}));
             })
             .catch(err => res.status(401).send({error: err}));
     })
