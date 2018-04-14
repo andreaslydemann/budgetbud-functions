0adaa57fe203a4607ba890ca1477cb8e6c5897a1
Victor Busk <victorbusk@hotmail.com>
2018-04-14 03:05:53 +0200

    New naming for eBanking function

--- functions/src/expenses/get_expenses_of_month.ts
+++ functions/src/expenses/get_expenses_of_month.ts
@@ -31,7 +31,7 @@ module.exports = function (req, res) {
         }
 
         const {data} =
-            await axios.get(`${EBANKING_FUNCTIONS_URL}/getExpenses?accountIDs=${accountIDs}&from=${dateInterval[0]}&to=${dateInterval[1]}`);
+            await axios.get(`${EBANKING_FUNCTIONS_URL}/getExpensesBetweenDates?accountIDs=${accountIDs}&from=${dateInterval[0]}&to=${dateInterval[1]}`);
 
         const filteredExpenses = expenseFetcher.filterExpenses(data);
 
