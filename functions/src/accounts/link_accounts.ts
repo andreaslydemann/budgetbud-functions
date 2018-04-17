0aa84a54580e42dde83b24e52c07156a7dab02b3
VictorBusk <victorbusk@hotmail.com>
2018-04-17 16:15:30 +0200

    linkAccounts race condition fixed

--- functions/src/accounts/link_accounts.ts
+++ functions/src/accounts/link_accounts.ts
@@ -3,44 +3,39 @@ import admin = require('firebase-admin');
 const cors = require('cors')({origin: true});
 
 module.exports = function (req, res) {
-    cors(req, res, () => {
+    cors(req, res, async () => {
         const token = req.get('Authorization').split('Bearer ')[1];
-        admin.auth().verifyIdToken(token)
-            .then(() => {
-                // Verify that the user provided an income
-                if (!req.body.eBankingAccIDs || !req.body.userID)
-                    return res.status(422).send({error: 'Fejl i anmodningen.'});
+        try {
+            await admin.auth().verifyIdToken(token);
+        } catch (err) {
+            res.status(401).send({error: "Brugeren kunne ikke verificeres."});
+        }
+        // Verify that the user provided an income
+        if (!req.body.eBankingAccIDs || !req.body.userID)
+            return res.status(422).send({error: 'Fejl i anmodningen.'});
 
-                const db = admin.firestore();
-                const userID = String(req.body.userID);
-                const eBankingAccIDs = req.body.eBankingAccIDs;
+        const db = admin.firestore();
+        const userID = String(req.body.userID);
+        const eBankingAccIDs = req.body.eBankingAccIDs;
 
-                // Create new accounts using the userID and accountID from the eBankingData
-                const deleteAccountsPromises = [];
-                db.collection('linkedAccounts')
-                    .where("userID", "==", userID)
-                    .get()
-                    .then((querySnapshot) => {
-                        const deleteAccountsPromise = querySnapshot.forEach((doc) => {
-                            doc.ref.delete();
-                        });
-                        deleteAccountsPromises.push(deleteAccountsPromise)
-                    });
+        // Create new accounts using the userID and accountID from the eBankingData
+        await db.collection('linkedAccounts')
+            .where("userID", "==", userID)
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    doc.ref.delete();
+                });
+            });
 
-                Promise.all(deleteAccountsPromises)
-                    .then(() => {
-                        eBankingAccIDs.forEach(accountDoc => {
-                            const id = String(accountDoc);
-                            db.collection('linkedAccounts').doc(id).set({
-                                userID
-                            })
-                                .then(() => res.status(200).send({success: true}))
-                                .catch(err => res.status(422)
-                                    .send({error: 'Kunne ikke oprette konti.'}));
-                        });
-                        res.status(200).send({success: true});
-                    })
+        eBankingAccIDs.forEach(accountDoc => {
+            const id = String(accountDoc);
+            db.collection('linkedAccounts').doc(id).set({
+                userID
             })
-            .catch(err => res.status(401).send({error: err}));
+                .then(() => res.status(200).send({success: true}))
+                .catch(err => res.status(422)
+                    .send({error: 'Kunne ikke oprette konti.'}));
+        });
     })
 };
