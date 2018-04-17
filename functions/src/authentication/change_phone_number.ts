2212a3551193cc4e6d524037ab02c06138f4e9de
Andreas Lüdemann <andreaslydemann@gmail.com>
2018-04-17 18:46:31 +0200

    Add functions for changing phone number and code

---
+++ functions/src/authentication/change_phone_number.ts
@@ -0,0 +1,30 @@
+import admin = require('firebase-admin');
+const cors = require('cors')({origin: true});
+
+module.exports = function (req, res) {
+    cors(req, res, async () => {
+        const token = req.get('Authorization').split('Bearer ')[1];
+        try {
+            await admin.auth().verifyIdToken(token);
+        } catch (err) {
+            res.status(401).send({error: "Brugeren kunne ikke verificeres."})
+        }
+
+        if (!req.body.cprNumber || !req.body.phoneNumber)
+            return res.status(400).send({error: 'Fejl i indtastning.'});
+
+        const cprNumber = String(req.body.cprNumber);
+        const phoneNumber = String(req.body.phoneNumber);
+
+        const db = admin.firestore();
+
+        try {
+            const userDoc = await db.collection("users").doc(cprNumber).get();
+
+            await userDoc.ref.update({phoneNumber});
+            return res.status(200).send({success: true});
+        } catch (err) {
+            res.status(401).send({error: "Brugeren kunne ikke verificeres."})
+        }
+    });
+};
