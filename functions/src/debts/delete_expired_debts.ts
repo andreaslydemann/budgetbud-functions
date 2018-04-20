918e21d1d256569d2ea6f261fe764dfaaa4121e5
Andreas Lüdemann <andreaslydemann@gmail.com>
2018-04-20 17:54:52 +0200

    Fix deleteExpiredDebts functions

--- functions/src/debts/delete_expired_debts.ts
+++ functions/src/debts/delete_expired_debts.ts
@@ -7,63 +7,68 @@ module.exports = function (req, res) {
     cors(req, res, async () => {
         const db = admin.firestore();
 
-        if (!req.query.cronKey)
+        if (!req.body.cronKey)
             return res.status(400).send({error: 'Fejl i anmodningen.'});
 
-        const callersCronKey = req.query.cronKey;
+        const callersCronKey = req.body.cronKey;
         const cronKey = functions.config().cron.key;
 
         if (callersCronKey !== cronKey)
             res.status(422).send({error: 'Cron key matchede ikke.'});
 
+        let debtsRef;
         let debts;
         try {
-            debts = await db.collection("debts").get();
+            debtsRef = db.collection("debts");
+            debts = await debtsRef.get();
         } catch (err) {
             res.status(422).send({error: 'Kunne ikke hente gæld.'});
         }
 
-        for (const debtDoc of debts) {
-            if (new Date() <= new Date(debtDoc.data().expirationDate)) {
-                let categoryDebts;
-                try {
-                    categoryDebts = await db.collection("categoryDebts")
-                        .where("debtID", "==", debtDoc.id)
-                        .get();
-                } catch (err) {
-                    res.status(422).send({error: 'Fejl opstod under gældssletningen.'});
-                }
+        for (let index in debts.docs) {
+            if (new Date() <= new Date(debts.docs[index].data().expirationDate))
+                continue;
 
-                const getCategoriesPromises = [];
-
-                categoryDebts.forEach(categoryDebtDoc => {
-                    const returnAmountsPromise = db.collection("categories")
-                        .doc(categoryDebtDoc.data().categoryID)
-                        .get();
-
-                    getCategoriesPromises.push(returnAmountsPromise);
-                });
-
-                const values = await Promise.all(getCategoriesPromises);
-                const returnAmountsPromises = [];
-
-                values.forEach(categoryDoc => {
-                    const categoryDebtDoc = categoryDebts.docs.filter((obj) => {
-                        return obj.data().categoryID === categoryDoc.id;
-                    });
-
-                    const returnAmountsPromise = categoryDoc.ref.update({
-                        amount: (categoryDoc.data().amount + categoryDebtDoc[0].data().amount)
-                    }).catch(() => res.status(422)
-                        .send({error: 'Fejl opstod under gældssletningen'}));
-
-                    returnAmountsPromises.push(returnAmountsPromise);
-                    returnAmountsPromises.push(categoryDebtDoc[0].ref.delete());
-                });
-
-                await Promise.all(returnAmountsPromises);
-                res.status(200).send({success: true});
+            let categoryDebts;
+            try {
+                categoryDebts = await db.collection("categoryDebts")
+                    .where("debtID", "==", debts.docs[index].id)
+                    .get();
+            } catch (err) {
+                res.status(422).send({error: 'Fejl opstod under gældssletningen.'});
             }
+
+            const getCategoriesPromises = [];
+
+            categoryDebts.forEach(categoryDebtDoc => {
+                const returnAmountsPromise = db.collection("categories")
+                    .doc(categoryDebtDoc.data().categoryID)
+                    .get();
+
+                getCategoriesPromises.push(returnAmountsPromise);
+            });
+
+            const values = await Promise.all(getCategoriesPromises);
+            const promises = [];
+
+            values.forEach(categoryDoc => {
+                const categoryDebtDoc = categoryDebts.docs.filter((obj) => {
+                    return obj.data().categoryID === categoryDoc.id;
+                });
+
+                const returnAmountsPromise = categoryDoc.ref.update({
+                    amount: (categoryDoc.data().amount + categoryDebtDoc[0].data().amount)
+                }).catch(() => res.status(422)
+                    .send({error: 'Fejl opstod under gældssletningen'}));
+
+                promises.push(returnAmountsPromise);
+                promises.push(categoryDebtDoc[0].ref.delete());
+            });
+
+            promises.push(debtsRef.doc(debts.docs[index].id).delete());
+            await Promise.all(promises);
         }
+
+        res.status(200).send({success: true});
     });
 };
